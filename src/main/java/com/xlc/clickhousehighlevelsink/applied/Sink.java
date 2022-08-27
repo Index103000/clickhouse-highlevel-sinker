dd7908227fda99e649dc50087544b20dd6a9cac7
xlvchao <33950671+xlvchao@users.noreply.github.com>
2022-08-27 11:40:33 +0800

    Delete src/main/java/com/xlc directory

--- src/main/java/com/xlc/clickhousehighlevelsink/applied/Sink.java
+++
@@ -1,7 +0,0 @@
-package com.xlc.clickhousehighlevelsink.applied;
-
-import java.util.concurrent.ExecutionException;
-
-public interface Sink extends AutoCloseable {
-    void put(String message) throws ExecutionException, InterruptedException;
-}
